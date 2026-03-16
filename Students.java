3efd4def694e758aaedcfe7453dcaef535b0369d
Ayesha <muhammadmohid621@gmail.com>
2026-03-16 10:50:16 +0500

    Apply OOP Concepts

---
+++ Students.java
@@ -0,0 +1,13 @@
+public class Students
+ {
+
+    String name;
+    int rollNo;
+
+    public void displayInfo(){
+        System.out.println("Student name is: "+ name);
+        System.out.println("Roll Number is: "+ rollNo);
+
+        System.out.println("-------------------------");
+    }    
+}
