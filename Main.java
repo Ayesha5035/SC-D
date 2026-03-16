180beb0bcedcad99a67f30e5715880eea59615e1
Ayesha <muhammadmohid621@gmail.com>
2026-03-16 09:20:17 +0500

    oop concept

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[]args){
+
+        Students student1 = new Students();
+        student1.name = "Ayesha";
+        student1.rollNo = 5035 ;
+
+        Students student2 = new Students();
+        student2.name = "Farooq";
+        student2.rollNo = 14 ;
+
+        student1.displayInfo();
+        student2.displayInfo();
+
+  } 
+    
+}
---
+++ Students.java
@@ -0,0 +1,12 @@
+public class Students {
+
+    String name;
+    int rollNo;
+
+    public void displayInfo(){
+        System.out.println("Student name is: "+ name);
+        System.out.println("Roll Number is: "+ rollNo);
+
+        System.out.println("-------------------------");
+    }    
+}
